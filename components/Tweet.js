ea529ba2d1bd981f95cc8d44b1da9ac4cf5f94dc
Maxi Ferreira <charca@gmail.com>
2016-01-19 20:58:23 -0800

    Added moment and text parser. Tweet component style

--- components/Tweet.js
+++ components/Tweet.js
@@ -8,6 +8,8 @@ let {
   Text
 } = React;
 
+let ParsedText = require('./ParsedText');
+let moment = require('moment');
 let globals = require('../globals');
 
 class Tweet extends React.Component {
@@ -18,19 +20,31 @@ class Tweet extends React.Component {
     let data = this.props.data;
 
     return (
-      <View style={styles.container}>
-        <Image style={styles.image} source={{uri: data.user.profile_image_url}} />
-        <View style={styles.tweet}>
-          <Text style={styles.author}>{data.user.name}</Text>
-          <Text style={styles.text}>{data.text}</Text>
+      <View>
+        <View style={styles.content}>
+          <Image style={styles.image} source={{uri: data.user.profile_image_url}} />
+          <View style={styles.tweet}>
+            <View style={styles.tweetHeader}>
+              <Text style={styles.author}>{data.user.name} <Text style={styles.handler}>@{data.user.screen_name}</Text></Text>
+              <Text style={styles.time}>{moment(new Date(data.created_at)).fromNow()}</Text>
+            </View>
+            <ParsedText
+              style={styles.text}
+              parse={[
+                { pattern: /#(\w+)/, style: styles.hashtag }
+              ]}>
+              { data.text }
+            </ParsedText>
+          </View>
         </View>
+        <View style={styles.separator}></View>
       </View>
     );
   }
 }
 
 let styles = StyleSheet.create({
-  container: {
+  content: {
     flexDirection: 'row',
     padding: 20
   },
@@ -43,11 +57,36 @@ let styles = StyleSheet.create({
   tweet: {
     flex: 1
   },
+  tweetHeader: {
+    flexDirection: 'row'
+  },
   author: {
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: '600',
     color: globals.colors.primary,
     marginBottom: 6
+  },
+  handler: {
+    fontSize: 12,
+    color: globals.colors.lightGrey
+  },
+  time: {
+    flex: 1,
+    fontSize: 12,
+    lineHeight: 16,
+    color: globals.colors.lightGrey,
+    textAlign: 'right'
+  },
+  text: {
+    fontSize: 14
+  },
+  hashtag: {
+    color: globals.colors.secondary
+  },
+  separator: {
+    height: .5,
+    backgroundColor: globals.colors.lightGrey,
+    marginBottom: 15
   }
 });
 
